0755783d05791dacbacfc9488e096faaedd87853
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-10 04:27:51 -0500

    Se agregaron los cambios para la plantilla

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,6 +15,24 @@ namespace soportec
     {
 
         /// <summary>
+        /// Control TitleContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder TitleContent;
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
         /// Control MainContent.
         /// </summary>
         /// <remarks>
